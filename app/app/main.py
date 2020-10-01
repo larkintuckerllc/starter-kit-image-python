13a29bb70842addfe6f66c26d97a2ee0e96c228c
John Tucker <john@larkintuckerllc.com>
2020-09-30 20:38:25 -0400

    module

--- app/app/main.py
+++ app/app/main.py
@@ -1,13 +1,12 @@
 from flask import Flask
 
+from app.mypackage.mymodule import myfunction
+
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 
 @app.route('/<path:path>')
 def hello_world(path):
-    return 'Hello, World!'
-
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    response = myfunction()
+    return response
