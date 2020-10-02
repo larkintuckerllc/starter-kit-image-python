82b12edd71f1a77edc8c4c4e37acd7b081eb964a
John Tucker <john@larkintuckerllc.com>
2020-10-02 14:07:49 -0400

    lint

--- app/app/main.py
+++ app/app/main.py
@@ -4,8 +4,8 @@ from app.mypackage import mymodule
 
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
 
+@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def hello_world(path):
     response = mymodule.myfunction()
